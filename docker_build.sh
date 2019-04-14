1c2c3849b6ce6d8ba143827c628a6c5d78ab4a61
cerealkill <cerealkill@users.noreply.github.com>
2019-04-14 19:49:27 +0200

    changed possibly conflicting port

--- docker_build.sh
+++ docker_build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #docker build -t ew-link-config-api .
-#pipenv lock -r > requirements.txt
+pipenv lock -r > requirements.txt
 docker build -t ew-link-config-api -f Dockerfile.alpine .
